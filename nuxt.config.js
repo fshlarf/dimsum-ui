859d6cee055cfdfb96517e8e94d573d61239ef5c
ibnuhazar101 <ibnuhazar101@gmail.com>
2025-08-09 11:34:17 +0700

    Fix dimsum-ui server configuration for Railway deployment

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,10 @@ export default {
   router: {
     base: '/',
   },
+  server: {
+    port: process.env.PORT || 3000,
+    host: '0.0.0.0'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
